8e29caab702e7a87c9a895a68b7d0ceb74b3bba8
BhanHG <96505078+BhanHG@users.noreply.github.com>
2023-01-13 16:37:57 -0700

    Added CurlScript.py

---
+++ Lab 1/CurlScript.py
@@ -0,0 +1,9 @@
+import os
+# os.system('pip show requests')
+# os.system('curl -i http://google.com/')
+# os.system('curl -iL http://google.com/')
+# os.system('curl -iL https://www.google.com/teapot')
+# os.system('curl -iL http://google.com/teapot')
+# os.system('curl -iL http://www.google.com/teapot')
+os.system('curl -i https://webdocs.cs.ualberta.ca/~hindle1/1.py')
+os.system('curl -i https://webdocs.cs.ualberta.ca/~hindle1/1.py -X POST -d "X=Y"')
